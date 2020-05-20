10d7b58f714f20b229bda2cc9fccff67ac6fb6c4
FONQRI <behnamsabaghi@gmail.com>
2020-05-20 23:31:04 +0430

    fixed to work

--- main.cpp
+++ main.cpp
@@ -8,6 +8,27 @@
 #include <sstream>
 #include <string>
 
+/**
+   * @brief  Convert const char* to size_t
+   * @note   When there is an error it returns the maximum of size_t
+   * @param  *number: const char*
+   * @retval size_t
+   */
+size_t to_size_t(const char* number)
+{
+    size_t sizeT;
+    std::istringstream iss(number);
+    iss >> sizeT;
+    if (iss.fail())
+    {
+        return std::numeric_limits<size_t>::max();
+    }
+    else
+    {
+        return sizeT;
+    }
+}
+
 using boost::asio::ip::udp;
 
 class udp_server
@@ -27,7 +48,7 @@ private:
     udp::socket _sock;
     boost::array<char, 256> _recv_buffer;
     std::string _write_message;
-    std::list<udp::endpoint> _endpoint_list;
+    std::vector<udp::endpoint> _endpoint_list;
     udp::endpoint _remote_endpoint; //current endpoint
 };
 
@@ -44,71 +65,48 @@ void udp_server::handle_receive(const boost::system::error_code&, std::size_t le
     std::string receive_message(_recv_buffer.data(), len);
     if (strcmp(receive_message.c_str(), "login") == 0)
     {
-        std::clog << __FUNCTION__ << __LINE__ << std::endl;
-        int isLogged = 0;
-        std::clog << __FUNCTION__ << __LINE__ << std::endl;
-        std::clog << __FUNCTION__ << __LINE__ << _endpoint_list.size() << std::endl;
-        for (std::list<udp::endpoint>::iterator iter = _endpoint_list.begin();
-             iter != _endpoint_list.end(); ++iter)
+        if (std::find(_endpoint_list.begin(), _endpoint_list.end(), _remote_endpoint)
+            != _endpoint_list.end())
         {
-            std::clog << __FUNCTION__ << __LINE__ << std::endl;
-            if (*iter == _remote_endpoint)
-            {
-                std::clog << __FUNCTION__ << __LINE__ << std::endl;
-                session_send(_remote_endpoint, "You have already logged in.\n");
-                isLogged = 1;
-                break;
-            }
-            std::clog << __FUNCTION__ << __LINE__ << std::endl;
-            if (!isLogged)
-            {
-                std::clog << __FUNCTION__ << __LINE__ << std::endl;
-                std::cout << "User login.\nAddress : " << _remote_endpoint.address().to_string()
-                          << std::endl;
-                std::clog << __FUNCTION__ << __LINE__ << std::endl;
-                std::cout << "Port : " << _remote_endpoint.port() << std::endl;
-                std::clog << __FUNCTION__ << __LINE__ << std::endl;
-                _endpoint_list.push_back(_remote_endpoint);
-                std::clog << __FUNCTION__ << __LINE__ << std::endl;
-                session_send(_remote_endpoint, "login success.\n");
-                std::clog << __FUNCTION__ << __LINE__ << std::endl;
-            }
-        }
 
-        std::clog << __FUNCTION__ << __LINE__ << std::endl;
+            session_send(_remote_endpoint, "You have already logged in.\n");
+        }
+        else
+        {
+            std::cout << "User login.\nAddress : " << _remote_endpoint.address().to_string()
+                      << std::endl;
+            std::cout << "Port : " << _remote_endpoint.port() << std::endl;
+            _endpoint_list.push_back(_remote_endpoint);
+            session_send(_remote_endpoint, "login success.\n");
+        }
     }
     else if (strcmp(receive_message.c_str(), "logout") == 0)
     {
-        int isLogged = 0;
-        for (std::list<udp::endpoint>::iterator iter = _endpoint_list.begin();
-             iter != _endpoint_list.end(); ++iter)
+        if (auto it = std::find(_endpoint_list.begin(), _endpoint_list.end(), _remote_endpoint);
+            it != _endpoint_list.end())
         {
-            if (*iter == _remote_endpoint)
-            {
-                isLogged = 1;
-                _endpoint_list.erase(iter);
-                std::cout << "User logout.\nAddress : " << _remote_endpoint.address().to_string()
-                          << std::endl;
-                std::cout << "Port : " << _remote_endpoint.port() << std::endl;
-                session_send(_remote_endpoint, "Logout success.\n");
-                break;
-            }
+            _endpoint_list.erase(it);
+            std::cout << "User logout.\nAddress : " << _remote_endpoint.address().to_string()
+                      << std::endl;
+            std::cout << "Port : " << _remote_endpoint.port() << std::endl;
+            session_send(_remote_endpoint, "Logout success.\n");
         }
-        if (!isLogged)
+        else
+        {
             session_send(_remote_endpoint, "Logout failed, you have not logged in.\n");
+        }
     }
     else if (strcmp(receive_message.c_str(), "list") == 0)
     {
         std::ostringstream message;
         int i = 0;
-        for (std::list<udp::endpoint>::iterator iter = _endpoint_list.begin();
-             iter != _endpoint_list.end(); ++iter)
+        for (auto iter : _endpoint_list)
         {
-            if (*iter == _remote_endpoint)
-                message << "[" << i << "]" << iter->address().to_string() << ":" << iter->port()
+            if (iter == _remote_endpoint)
+                message << "[" << i << "]" << iter.address().to_string() << ":" << iter.port()
                         << " (yourself)" << std::endl;
             else
-                message << "[" << i << "]" << iter->address().to_string() << ":" << iter->port()
+                message << "[" << i << "]" << iter.address().to_string() << ":" << iter.port()
                         << std::endl;
             i++;
         }
@@ -116,23 +114,26 @@ void udp_server::handle_receive(const boost::system::error_code&, std::size_t le
     }
     else if (strncmp(receive_message.c_str(), "punch", 5) == 0)
     {
-        int punched_client = atoi(receive_message.c_str() + 6);
-        std::list<udp::endpoint>::iterator iter = _endpoint_list.begin();
-        for (int i = 0; i < punched_client && iter != _endpoint_list.end(); ++i, ++iter)
-            ;
+        std::clog << __FUNCTION__ << __LINE__ << std::endl;
+
+        size_t punched_client = to_size_t(receive_message.c_str() + 6);
+
         std::ostringstream message;
-        if (iter == _endpoint_list.end())
-            message << "Punch failed, no such client.";
-        else
+        if (_endpoint_list.size() > punched_client)
         {
             std::ostringstream peer_message;
             //udp::endpoint peer_endpoint(iter->address(), iter->port);
             peer_message << "PUNCH_REQUEST " << _remote_endpoint.address().to_string() << ":"
                          << _remote_endpoint.port() << std::endl;
-            session_send(*iter, peer_message.str());
-            message << "PUNCH_SUCCESS " << iter->address().to_string() << ":" << iter->port()
-                    << std::endl;
+            session_send(_endpoint_list[punched_client], peer_message.str());
+            message << "PUNCH_SUCCESS " << _endpoint_list[punched_client].address().to_string()
+                    << ":" << _endpoint_list[punched_client].port() << std::endl;
         }
+        else
+        {
+            message << "Punch failed, no such client.";
+        }
+
         session_send(_remote_endpoint, message.str());
     }
     else if (strcmp(receive_message.c_str(), "help") == 0)
@@ -167,6 +168,7 @@ void udp_server::handle_send(const boost::system::error_code& ec, std::size_t le
 
 int main(int argc, char* argv[])
 {
+
     boost::asio::io_service io_service;
     udp_server server(io_service);
     io_service.run();
